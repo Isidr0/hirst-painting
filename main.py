55bfed16562a04306077aecedb6bcd8d602f3c73
Isidr0 <rtwatson86@gmail.com>
2021-11-17 13:47:21 -0500

    completed painting

--- main.py
+++ main.py
@@ -21,6 +21,7 @@
 # print(rgb_colors)
 
 from turtle import Turtle, Screen
+import random
 
 color_list = [(234, 251, 243), (197, 13, 32), (249, 237, 21), (39, 77, 188), (238, 227, 5), (39, 216, 68),
               (228, 160, 47), (243, 247, 253), (28, 40, 155), (214, 75, 14), (16, 153, 17), (199, 15, 11),
@@ -30,8 +31,52 @@ color_list = [(234, 251, 243), (197, 13, 32), (249, 237, 21), (39, 77, 188), (23
 
 tim = Turtle()
 
-# paint 10x10 dots. 20 in size. spaced 50 paces apart.
+# hirst painting: paint 10x10 dots. 20 in size. spaced 50 paces apart.
+
+tim.speed ('fastest')
+Screen().colormode(255)
+
+tim.penup()
+tim.hideturtle()
+ypos = -250
+xpos = -250
+tim.setx(xpos)
+tim.sety(ypos)
 
 
+# def turtle_dot():
+
+def tim_forward():
+    for _ in range(10):
+        random_color = (random.choice(color_list))
+        tim.dot(20, random_color)
+        tim.forward(50)
+
+
+for _ in range(10):
+    tim_forward()
+    ypos += 50
+    tim.setx(xpos)
+    tim.sety(ypos)
+
+# def tim_left():
+#     tim.left(90)
+#     tim.forward(50)
+#     tim.left(90)
+#
+# def tim_right():
+#     tim.right(90)
+#     tim.forward(50)
+#     tim.right(90)
+#
+# for _ in range(5):
+#     # turtle_dot()
+#     print(tim.heading())
+#     tim_forward()
+#     tim_left()
+#     tim_forward()
+#     tim_right()
+#     print(tim.pos())
+#     print(tim.heading())
 
 Screen().exitonclick()
